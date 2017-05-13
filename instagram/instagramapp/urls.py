2554fc0a7fa8201bef6e57fc6cf8443e943625a6
Centro Ático <centroatico@202C11E27168972.local>
2017-05-13 12:29:14 -0500

    Version 1.2

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^', views.registrar, name = 'registrar'),
-    url(r'^Inicio/', views.iniciar, name = 'iniciar')
+    url(r'^$', views.registrar, name = 'registrar'),
+    url(r'^inicio/$', views.iniciar, name = 'iniciar'),
+    url(r'^home/$', views.home, name = 'home')
 ]
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def registrar(request):
     return render(request, 'registro.html')
-    
+
 def iniciar(request):
     return render(request, 'Inicio.html')
+
+def home(request):
+    return render(request, 'Ins.html')
